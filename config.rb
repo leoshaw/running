3d4961c3469d1f50cce790ebc0e36dda6a7f8cc1
Leo Shaw <leo@leoshaw.co.uk>
2015-07-17 23:40:59 +0100

    Generate routes from data file

--- config.rb
+++ config.rb
@@ -27,6 +27,11 @@
 # Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
+data.routes.each do |route|
+  proxy "/routes/#{route.page_name}.html", "/routes/template.html", :locals => { :local_data => route }, :ignore => true do
+    @title = route[:title]
+  end
+end
 
 ###
 # Helpers
